2942c3e8d65c6c51d9f5d515877d9991b2284916
ken <proxy.vk@gmail.com>
2012-05-04 16:44:01 +0800

    add xsl

--- dblatex/a2x_ch.sh
+++ dblatex/a2x_ch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a2x -f pdf --dblatex-opts='-b xetex -s ~/asciidoc-dblatex.sty' $*
-a2x -v -f pdf --dblatex-opts='-b xetex -s ~/asciidoc-dblatex.sty -I /usr/share/dblatex/latex/graphics ' $*
+a2x --icons -k -v -f pdf --dblatex-opts='-d -b xetex -s ~/asciidoc-dblatex.sty -I /usr/share/dblatex/latex/graphics -p ~/asciidoc-dblatex.xsl ' $*
